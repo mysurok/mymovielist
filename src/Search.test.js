2af7a65fc9e5e50d86f0663d2cf16a7b1c64d07a
nataliamazurskaya <natalia.mazurskaya@eniro.com>
2019-08-28 19:50:32 +0200

    Added several tests.

--- src/Search.test.js
+++ src/Search.test.js
@@ -1,36 +1,112 @@
 import reducer, { initialState } from './reducers/SearchBox'
-import {SEARCH_COMPLETED, SEARCH_COMPLETED_WITH_ERROR} from "./actions/SearchBox"
-
-//SEARCH_COMPLETED, SEARCH_COMPLETED_WITH_ERROR
+import {
+    SEARCH_COMPLETED,
+    SEARCH_BY_ID_COMPLETED,
+    SEARCH_COMPLETED_WITH_ERROR,
+    completeSearch
+} from "./actions/SearchBox"
 
 describe('search reducer', () => {
 
     it('SEARCH_COMPLETED', () => {
         const action = {
             type: SEARCH_COMPLETED,
-            payload: { searchResult: {id: "76341", original_title: "Mad Max: Fury Road"}, query: "76341" },
+            payload: { searchResult: {id: "76341", original_title: "Mad Max: Fury Road"}, query: "Mad Max" },
         }
 
         expect(reducer(initialState, action)).toEqual({
             ...initialState,
             searchError: null,
-            searchResult: action.payload.searchResult,
+            movie: null,
+            list: action.payload.searchResult,
             query: action.payload.query
         })
     })
 
-    it('SEARCH_COMPLETED_WITH_ERROR', () => {
+    it('SEARCH_BY_ID_COMPLETED', () => {
+        const initialState = {
+            list: {results: []},
+            searchError: null,
+            movie: null
+        }
         const action = {
-            type: SEARCH_COMPLETED_WITH_ERROR,
-            payload: { searchError: {status_code: 34, status_message: "Error 404"}, query: "7634" },
+            type: SEARCH_BY_ID_COMPLETED,
+            payload: { searchResult: {id: "76341", original_title: "Mad Max: Fury Road"}},
         }
 
         expect(reducer(initialState, action)).toEqual({
             ...initialState,
-            searchResult: null,
+            searchError: null,
+            movie: action.payload.searchResult
+        })
+    })
+
+    it('SEARCH_COMPLETED_WITH_ERROR', () => {
+        const initialState = {
+            list: {results: []},
+            searchError: null,
+            movie: {id: "76341", original_title: "Mad Max: Fury Road"}
+        }
+        const action = {
+            type: SEARCH_COMPLETED_WITH_ERROR,
+            payload: { searchError: {status_code: 34, status_message: "Error 404"}, query: "7634" }
+        }
+
+        expect(reducer(initialState, action)).toEqual({
+            ...initialState,
+            list: null,
+            movie: null,
             query: action.payload.query,
             searchError: action.payload.searchError
         })
     })
 
+    it('SEARCH_COMPLETED', () => {
+        const initialState = {
+            list: null,
+            movie: null,
+            searchError: "404 Movie not found"
+        }
+        const action = {
+            type: SEARCH_COMPLETED,
+            payload: { searchResult: {id: "76341", original_title: "Mad Max: Fury Road"}, query: "Mad Max" },
+        }
+
+        expect(reducer(initialState, action)).toEqual({
+            ...initialState,
+            searchError: null,
+            movie: null,
+            list: action.payload.searchResult,
+            query: action.payload.query
+        })
+    })
+
+    it('SEARCH_BY_ID_COMPLETED', () => {
+        const initialState = {
+            list: null,
+            movie: null,
+            searchError: "404 Movie not found"
+        }
+        const action = {
+            type: SEARCH_BY_ID_COMPLETED,
+            payload: { searchResult: {id: "76341", original_title: "Mad Max: Fury Road"}},
+        }
+
+        expect(reducer(initialState, action)).toEqual({
+            ...initialState,
+            searchError: null,
+            movie: action.payload.searchResult
+        })
+    })
+
+    it('completeSearch: should create an action to set SEARCH_COMPLETED', () => {
+        const expectedAction = {
+            type: SEARCH_COMPLETED,
+            payload: {
+                searchResult: {id: "76341", original_title: "Mad Max: Fury Road"},
+                query: "Mad Max"
+            }
+        }
+        expect(completeSearch({id: "76341", original_title: "Mad Max: Fury Road"}, "Mad Max")).toEqual(expectedAction)
+    })
 })
